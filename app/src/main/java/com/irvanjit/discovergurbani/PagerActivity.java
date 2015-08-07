6f6d8286181c32b0cfc026dd43280161987004d3
Irvanjit Singh Gill <irvan.gill@gmail.com>
2015-08-06 23:10:17 -0400

    sehaj paath improvements

--- app/src/main/java/com/irvanjit/discovergurbani/PagerActivity.java
+++ app/src/main/java/com/irvanjit/discovergurbani/PagerActivity.java
@@ -2,6 +2,7 @@ package com.irvanjit.discovergurbani;
 
 import java.util.Locale;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
@@ -12,6 +13,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,8 +22,10 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class PagerActivity extends ActionBarActivity implements ActionBar.TabListener {
@@ -192,6 +196,9 @@ public class PagerActivity extends ActionBarActivity implements ActionBar.TabLis
 
         String translationId;
         String transliterationId;
+        String angNumber;
+        EditText angField;
+        Toast toast;
 
         public static ReadPageFragment newInstance() {
             ReadPageFragment fragment = new ReadPageFragment();
@@ -206,23 +213,40 @@ public class PagerActivity extends ActionBarActivity implements ActionBar.TabLis
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_page_read, container, false);
+
+            //setup error toast
+            Context context = getActivity().getApplicationContext();
+            CharSequence connError = "Please enter a valid ang number";
+            int duration = Toast.LENGTH_SHORT;
+            toast = Toast.makeText(context, connError, duration);
+
             setupSpinners(rootView);
             setupButton(rootView);
             return rootView;
         }
 
-        void setupButton(View view){
+        void setupButton(View view) {
+            angField = (EditText)view.findViewById(R.id.ang_number);
             Button button = (Button)view.findViewById(R.id.read_button);
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent in = new Intent(getActivity(), ShabadActivity.class);
-                    in.putExtra("hymn", "1");
-                    in.putExtra("id", -1);
-                    in.putExtra("translation", translationId);
-                    in.putExtra("transliteration", transliterationId);
-                    in.putExtra("displayMode", 1);
-                    startActivity(in);
+                    angNumber = angField.getText().toString();
+                    if (angNumber.isEmpty()) {
+                        angNumber = "1";
+                    }
+                    int ang = Integer.parseInt(angNumber);
+                    if (ang < 1 || ang > 1430) {
+                        toast.show();
+                    } else {
+                        Intent in = new Intent(getActivity(), ShabadActivity.class);
+                        in.putExtra("hymn", "1");
+                        in.putExtra("id", -1);
+                        in.putExtra("translation", translationId);
+                        in.putExtra("transliteration", transliterationId);
+                        in.putExtra("displayMode", 1);
+                        startActivity(in);
+                    }
                 }
             });
         }
